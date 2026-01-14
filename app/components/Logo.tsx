f1f331ac824937dd98a9a68d5c0b7e04e0038be3
xpand03 <raunek@xpandholdings.com>
2026-01-14 14:42:53 -0800

    Update login logo to use same grey background version as header, sized proportionally for sign-in page

--- app/components/Logo.tsx
+++ app/components/Logo.tsx
@@ -2,12 +2,12 @@ import Image from 'next/image'
 
 export default function Logo() {
   return (
-    <div className="w-20 h-20 rounded-full bg-[#f0f3fa] flex items-center justify-center mb-8 shadow-[inset_8px_8px_16px_#d1d9e6,inset_-8px_-8px_16px_#ffffff]">
+    <div className="flex items-center justify-center mb-8">
       <Image
-        src="/kat-logo.png"
+        src="/kat-logo-grey-bg.png"
         alt="The Leadership Development App"
-        width={80}
-        height={84}
+        width={120}
+        height={120}
         className="object-contain"
       />
     </div>
